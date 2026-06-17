fb585c4787201e9ade9cc6717ac778a9c858cf8e
Thomas Keil <thomas@weblizards.de>
2026-06-17 18:08:28 +0200

    [Tests] Added some more tests

--- tests/Unit/Service/StatusServiceTest.php
+++ tests/Unit/Service/StatusServiceTest.php
@@ -14,6 +14,96 @@ use Weblizards\CustomMaintenanceBundle\Service\StatusService;
 
 final class StatusServiceTest extends TestCase
 {
+    public function testManualActiveStatusIsEvaluatedFromCanonicalConfigOutsideTimeslot(): void
+    {
+        Carbon::setTestNow('2026-02-01 12:30');
+
+        $settingsStore = $this->createMock(ConfigPersistenceInterface::class);
+        $settingsStore
+            ->expects(self::once())
+            ->method('load')
+            ->willReturn($this->buildStatusServiceConfig(StatusService::STATUS_ACTIVE));
+
+        $legacyLoader = $this->createMock(LegacyConfigLoaderInterface::class);
+        $legacyLoader->expects(self::never())->method('load');
+
+        $service = new StatusService(new MaintenanceConfigManager($settingsStore, $legacyLoader), $this->createHeadLinkMock());
+
+        self::assertSame(StatusService::STATUS_ACTIVE, $service->getStatus('prices'));
+        self::assertTrue($service->isActive('prices'));
+
+        Carbon::setTestNow();
+    }
+
+    public function testManualInactiveStatusIsEvaluatedFromCanonicalConfigOutsideTimeslot(): void
+    {
+        Carbon::setTestNow('2026-02-01 12:30');
+
+        $settingsStore = $this->createMock(ConfigPersistenceInterface::class);
+        $settingsStore
+            ->expects(self::once())
+            ->method('load')
+            ->willReturn($this->buildStatusServiceConfig(StatusService::STATUS_INACTIVE));
+
+        $legacyLoader = $this->createMock(LegacyConfigLoaderInterface::class);
+        $legacyLoader->expects(self::never())->method('load');
+
+        $service = new StatusService(new MaintenanceConfigManager($settingsStore, $legacyLoader), $this->createHeadLinkMock());
+
+        self::assertSame(StatusService::STATUS_INACTIVE, $service->getStatus('prices'));
+        self::assertFalse($service->isActive('prices'));
+
+        Carbon::setTestNow();
+    }
+
+    public function testSetStatusPersistsAndIsReadBackThroughTheSameCanonicalConfigurationPath(): void
+    {
+        Carbon::setTestNow('2026-02-01 12:30');
+
+        $store = new class($this->buildStatusServiceConfig(StatusService::STATUS_INACTIVE)) implements ConfigPersistenceInterface {
+            private ?array $data;
+
+            public function __construct(?array $data)
+            {
+                $this->data = $data;
+            }
+
+            public function load(): ?array
+            {
+                return $this->data;
+            }
+
+            public function save(array $data): void
+            {
+                $this->data = $data;
+            }
+        };
+
+        $legacyLoader = new class() implements LegacyConfigLoaderInterface {
+            public function load(): ?array
+            {
+                return null;
+            }
+        };
+
+        $service = new StatusService(new MaintenanceConfigManager($store, $legacyLoader), $this->createHeadLinkMock());
+
+        self::assertSame(StatusService::STATUS_INACTIVE, $service->getStatus('prices'));
+        self::assertFalse($service->isActive('prices'));
+
+        $service->setStatus('prices', StatusService::STATUS_ACTIVE);
+
+        self::assertSame(StatusService::STATUS_ACTIVE, $service->getStatus('prices'));
+        self::assertTrue($service->isActive('prices'));
+
+        $service->setStatus('prices', StatusService::STATUS_INACTIVE);
+
+        self::assertSame(StatusService::STATUS_INACTIVE, $service->getStatus('prices'));
+        self::assertFalse($service->isActive('prices'));
+
+        Carbon::setTestNow();
+    }
+
     public function testServiceEvaluatesStatusesAgainstTheDomainModel(): void
     {
         Carbon::setTestNow('2026-02-01 10:30');
@@ -198,5 +288,55 @@ final class StatusServiceTest extends TestCase
         self::assertFalse($service->showUpcoming('search'));
 
         Carbon::setTestNow();
+    }
+
+    private function createHeadLinkMock(): HeadLink
+    {
+        $headLink = $this
+            ->getMockBuilder(HeadLink::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['__call'])
+            ->getMock();
+        $headLink
+            ->expects(self::once())
+            ->method('__call')
+            ->with('appendStylesheet', ['/bundles/weblizardscustommaintenance/css/frontend.css']);
+
+        return $headLink;
+    }
+
+    private function buildStatusServiceConfig(string $activeStatus): array
+    {
+        return [
+            'frontend' => [
+                'indication_upcoming' => ['de' => 'Upcoming %s %s'],
+                'indication_current' => ['de' => 'Current %s %s'],
+                'more' => ['de' => 'Mehr'],
+                'fulltimeformat' => ['de' => 'd.m.Y H:i'],
+            ],
+            'pimcore' => [
+                'show_info' => 'never',
+                'show_info_from' => ['date' => '01.01.2026', 'time' => '00:00'],
+                'planned' => [
+                    'from' => ['date' => '01.01.2026', 'time' => '00:00'],
+                    'to' => ['date' => '01.01.2026', 'time' => '01:00'],
+                ],
+                'document' => '',
+            ],
+            'custom' => [
+                'prices' => [
+                    'active' => $activeStatus,
+                    'fixed' => StatusService::STATUS_INACTIVE,
+                    'description' => 'ERP',
+                    'show_info' => 'always',
+                    'show_info_from' => ['date' => '01.02.2026', 'time' => '08:00'],
+                    'planned' => [
+                        'from' => ['date' => '01.02.2026', 'time' => '09:00'],
+                        'to' => ['date' => '01.02.2026', 'time' => '11:00'],
+                    ],
+                    'document' => '/de/erp',
+                ],
+            ],
+        ];
     }
 }
